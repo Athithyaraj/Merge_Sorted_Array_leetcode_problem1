1829d3e9ea8b5733dafd3df9ebd495a578a60f5b
Athithyaraj <adhimurali09@gmail.com>
2024-01-26 12:51:57 +0530

    cherrypick commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Merge_Sorted_Array_leetcode_problem1;
 class Program { 
     public static async Task Main(String[] args) 
     { 
+        Console.WriteLine("Welcome");
         var t1 = new testcase1();
         await t1.TestcaseAsync();
     } 
